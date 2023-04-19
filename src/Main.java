041d7fa4942c967a1e97bc13897c9894a6caf1d3
nivmaman11 <niv.maman@campus.technion.ac.il>
2023-04-19 22:27:26 +0100

    aded 2 funtions: print board and place battleships.

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    static final char CLEAR = '-';
+    static final char CLEAR = '–';
     static final char HIT = 'v';
     static final char MISS = 'x';
     static final char ALIVE = '#';
@@ -22,14 +22,15 @@ public class Main {
     /*
     * the function receive the board size from the user,
     * both row and col sizes
-    * @return arr - contains rownum in arr[0] and colnum in arr[1]
-    *  */
+    * @return arr - contains number of rows in arr[0] and number of columns in arr[1]
+    */
     public static int[] inputBoardSize(){
         System.out.println("Enter the board size");
         String boardSize = scanner.nextLine();
-        int rowNum = (int) boardSize.charAt(0) - 48;//converting into int
-        int colNum = (int) boardSize.charAt(2) - 48;//same
-        int [] arr = {rowNum, colNum};
+        String[]  str = boardSize.split("x");
+        int [] arr =new int[2];
+        arr[0] =  Integer.parseInt(str[0]);
+        arr[1] =  Integer.parseInt(str[1]);
         return arr;
     }
 
@@ -61,14 +62,46 @@ public class Main {
         String battleships = scanner.nextLine();
         return battleships;
     }
-    public static void input_battleships_placement(int[][] game_board, int n, int m, String battleshipsString){
+    public static void input_battleships_placement(char [][] game_board, int n, int m, String battleshipsString){
 
     }
-    public static void placeUserBattleships(){
+    /*
+    function that place the battleships on the board.
+    the function checks if the direction horizontal or vertical and place is accordingly in a loop.
+    @params: game board - 2d's arr; the game board
+            bslen - the length of the battleships that is being placed
+            x and y - the coordinates of the beginning of the battleship place.
+            dir - the direction that the battleship will be placed - vertical or horizontal
 
+     */
+    public static void placeUserBattleships( char [][] game_board, int bsLen, int x, int y, int dir ){
+        if(dir == 1){
+            for(int i = 0; i < bsLen; i++){
+                game_board[x][y + i] = ALIVE;
+            }
+        }
+        if(dir == 0){
+            for(int i = 0; i < bsLen; i++){
+                game_board[x + i][y] = ALIVE;
+            }
+        }
     }
-    public static void printBoard(){
-
+    /*
+    this function prints the board.
+    @param: n - number of rows
+            m - number of column
+            board- any kind of board
+     */
+    public static void printBoard(int n, int m, int[][] board){
+        System.out.print("  ");
+        for(int i = 0; i < m; i ++)
+            System.out.println(i);
+        for(int k = 0; k < n; k++){
+            System.out.print(k + " " );
+            for(int j = 0; j < m; j++){
+                System.out.print(board[k][j]);
+            }
+        }
     }
     public static void placeComputerBattleships(){
 
